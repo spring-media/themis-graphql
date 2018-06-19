17763b9dd1936482aecf206b0b74049ad7f16307
Feit, Michael <michael.feit@axelspringer.de>
2018-06-19 16:15:12 +0200

    RED-313 initial pact setup

---
+++ test/contract/article.test.pact.js
@@ -0,0 +1,54 @@
+/* global provider, port */
+/* eslint-disable max-nested-callbacks */
+
+'use strict';
+
+const getMeDogs = require('../index').getMeDogs;
+
+describe('Dog\'s API', () => {
+	const url = 'http://localhost';
+
+	const EXPECTED_BODY = [{
+		dog: 1,
+	}];
+
+	describe('works', () => {
+		beforeEach(() => {
+			const interaction = {
+				state: 'i have a list of projects',
+				uponReceiving: 'a request for projects',
+				withRequest: {
+					method: 'GET',
+					path: '/dogs',
+					headers: {
+						'Accept': 'application/json',
+					},
+				},
+				willRespondWith: {
+					status: 200,
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: EXPECTED_BODY,
+				},
+			};
+
+			return provider.addInteraction(interaction);
+		});
+
+		// add expectations
+		it('returns a sucessful body', done => {
+			return getMeDogs({
+				url,
+				port,
+			})
+				.then(response => {
+					expect(response.headers['content-type']).toEqual('application/json');
+					expect(response.data).toEqual(EXPECTED_BODY);
+					expect(response.status).toEqual(200);
+					done();
+				})
+				.then(() => provider.verify());
+		});
+	});
+});
