bc8ba85b35bc93b16f0feab3ca3eb5ce52bfb5da
kommander <hasta84@gmail.com>
2018-06-21 14:50:52 +0200

    Refactor error formatting

--- src/logger/index.js
+++ src/logger/index.js
@@ -2,10 +2,10 @@ const { Logger, transports } = require('winston');
 const isProd = process.env.NODE_ENV === 'production';
 
 const logger = new (Logger)({
+  level: process.env.LOG_LEVEL || 'error',
   transports: [
     new transports.Console({
       colorize: !isProd,
-      level: process.env.LOG_LEVEL || 'error',
     }),
   ],
 });
