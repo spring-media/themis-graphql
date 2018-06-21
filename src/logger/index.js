398c5f5449cb2fbf4be64fd30cdf98d5b0d91ceb
Feit, Michael <michael.feit@axelspringer.de>
2018-06-21 12:49:04 +0200

    review changes, rename const to isProd

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,10 +1,10 @@
 const { Logger, transports } = require('winston');
-const isDev = process.env.NODE_ENV !== 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 const logger = new (Logger)({
   transports: [
     new transports.Console({
-      colorize: isDev,
+      colorize: !isProd,
       level: process.env.LOG_LEVEL || 'error',
     }),
   ],
