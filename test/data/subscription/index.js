3507c6bf31cfc500d59a1c7a5e5224271c14db4f
kommander <hasta84@gmail.com>
2018-12-10 07:01:29 +0100

    Use startup and shutdown for subscription setup

--- test/data/subscription/index.js
+++ test/data/subscription/index.js
@@ -1,9 +1,37 @@
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const { PubSub } = require('apollo-server-express');
+let intervall = null;
+
+const pubsub = new PubSub();
+const WALLET_CHANGED = 'WALLET_CHANGED';
+
+const walletState = {
+  id: 'baf86a8bf86af8',
+  value: 0,
+};
 
 module.exports = {
   name: 'subscription',
   typeDefs,
   resolvers,
   mocks: {},
+  context () {
+    return {
+      pubsub,
+      eventType: {
+        WALLET_CHANGED,
+      },
+      walletState,
+    };
+  },
+  onStartup () {
+    intervall = setInterval(() => {
+      walletState.value += Math.round(Math.random() * 100);
+      pubsub.publish(WALLET_CHANGED, { wallet: walletState });
+    }, 2000);
+  },
+  onShutdown () {
+    clearInterval(intervall);
+  },
 };
--- test/data/subscription/resolvers.js
+++ test/data/subscription/resolvers.js
@@ -1,33 +1,14 @@
-const { PubSub } = require('apollo-server-express');
-
-const pubsub = new PubSub();
-const WALLET_CHANGED = 'WALLET_CHANGED';
-
-const walletState = {
-  id: 'baf86a8bf86af8',
-  value: 0,
-};
-
 module.exports = {
   Query: {
-    wallet: () => {
-      return walletState;
+    wallet: (p, arg, ctx) => {
+      return ctx.walletState;
     },
   },
   Subscription: {
     wallet: {
-      subscribe () {
-        return pubsub.asyncIterator([WALLET_CHANGED]);
+      subscribe (p, arg, ctx) {
+        return ctx.pubsub.asyncIterator([ctx.eventType.WALLET_CHANGED]);
       },
     },
   },
 };
-
-const intervall = setInterval(() => {
-  walletState.value += Math.round(Math.random() * 100);
-  pubsub.publish(WALLET_CHANGED, { wallet: walletState });
-}, 2000);
-
-process.on('SIGINT', () => {
-  clearInterval(intervall);
-});
