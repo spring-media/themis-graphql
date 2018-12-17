643b5f182ad477e4e4572117e347d6bb4ce8fa02
kommander <hasta84@gmail.com>
2018-12-17 22:10:26 +0100

    Expose query loading tool

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { initServer } = require('./server');
 const { buildDatasource } = require('./build-datasource');
 const { setupDatasource } = require('./setup-datasource');
+const { loadFileQuery } = require('./load-query');
+const { loadFileConfig } = require('./load-file-config');
 const graphql = require('graphql');
 const tools = require('graphql-tools');
 const gql = require('graphql-tag');
@@ -14,4 +16,6 @@ module.exports = {
   graphql,
   tools,
   supertest,
+  loadFileQuery,
+  loadFileConfig,
 };
--- src/load-query.test.js
+++ src/load-query.test.js
@@ -1,9 +1,10 @@
-const { loadQuery } = require('./load-query');
+const { loadFileQuery } = require('./load-query');
 const path = require('path');
 
 describe('load-query', () => {
   it('handles a file with recursive #import statements', async () => {
-    const result = await loadQuery(path.resolve(__dirname, '../test/data/queries/start.gql'));
+    const result = await loadFileQuery(path.resolve(__dirname, '../test/data/queries/start.gql'));
+
     expect(result).toMatchSnapshot();
   });
 });
