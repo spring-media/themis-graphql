1ea5ccef3390fa3398b0f16f6309679e5b20f4c1
David Ruisinger <david.ruisinger@axelspringer.com>
2019-03-06 15:27:35 +0100

    Added support for passing custom regexForTestFiles to jest

--- src/run-tests.js
+++ src/run-tests.js
@@ -3,13 +3,16 @@ const path = require('path');
 
 const runTests = () => {
   // Remove arguments before --test to not collide with jest args
-  const testArgIndex = process.argv.findIndex(arg => /-t|--test/.test(arg));
-  const argv = [ ...process.argv.slice(0, 2), ...process.argv.slice(testArgIndex, process.argv.length) ]
-    .filter(arg => ![ '-t', '--test', '--record' ].includes(arg))
-    .concat([
-      '--setupTestFrameworkScriptFile', path.resolve(__dirname, '../test/framework.js'),
-      './',
-    ]);
+  const testArgIndex = process.argv.findIndex(arg => /-t$|--test$/.test(arg));
+  const argv = [
+    ...process.argv.slice(0, 2),
+    `--setupTestFrameworkScriptFile=${path.resolve(
+      __dirname,
+      '../test/framework.js'
+    )}`,
+    ...process.argv.slice(testArgIndex, process.argv.length),
+    ...(/^--/.test(process.argv.slice(-1)[0]) ? ['./'] : []),
+  ].filter(arg => ![ '-t', '--test', '--record' ].includes(arg));
 
   jestCLI.run(argv);
 };
