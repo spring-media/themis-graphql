66c04f14ce361c8de575a4af5b483deb7d61a756
Markus Kaiserswerth <github@sensun.org>
2019-06-07 17:07:05 +0200

    Fixed log message metadata not being logged

--- src/logger/utils.js
+++ src/logger/utils.js
@@ -1,20 +1,37 @@
 const { Logger, transports, config } = require('winston');
 const { isProd } = require('../utils');
 
+const formatMessage = ({ level, message, meta = {}, colorize }) => {
+  let result = colorize ? config.colorize(level) : level;
+
+  // Prepend the request ID to the message
+  // eslint-disable-next-line newline-after-var
+  let messageStr = message;
+  if (meta.requestId) {
+    messageStr = `[${meta.requestId}] ${messageStr}`;
+  }
+
+  result += ` ${messageStr}`;
+
+  // Add the metadata (if any), but omit the request ID
+  // eslint-disable-next-line newline-after-var
+  const otherMeta = { ...meta };
+  delete otherMeta.requestId;
+
+  if (Object.entries(otherMeta).length) {
+    result += ` ${JSON.stringify(otherMeta)}`;
+  }
+
+  return result;
+};
+
 const createLogger = options => {
   return new (Logger)({
     level: process.env.LOG_LEVEL || 'info',
     transports: [
       new transports.Console({
-        formatter: ({ level, message, meta = {} }) => {
-          const levelStr = !isProd ? config.colorize(level) : level;
-
-          if (meta.requestId) {
-            return `${levelStr} [${meta.requestId}] ${message}`;
-          }
-
-          return `${levelStr} ${message}`;
-        },
+        colorize: !isProd,
+        formatter: formatMessage,
       }),
     ],
     ...options,
