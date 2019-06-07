214124d8b29244e69e53a4ee47974d97fd99aea4
Markus Kaiserswerth <github@sensun.org>
2019-06-07 14:55:13 +0200

    Added log formatter to include request ID if given

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,10 +1,18 @@
-const { Logger, transports } = require('winston');
+const { Logger, transports, config } = require('winston');
 const { isProd } = require('../utils');
 const logger = new (Logger)({
   level: process.env.LOG_LEVEL || 'info',
   transports: [
     new transports.Console({
-      colorize: !isProd,
+      formatter: ({ level, message, meta = {} }) => {
+        const levelStr = !isProd ? config.colorize(level) : level;
+
+        if (meta.requestId) {
+          return `${levelStr} [${meta.requestId}] ${message}`;
+        }
+
+        return `${levelStr} ${message}`;
+      },
     }),
   ],
 });
