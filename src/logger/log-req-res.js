90e7ceb6c14ba79659866279bedb6e7d5c6896ab
Markus Kaiserswerth <github@sensun.org>
2019-06-07 13:55:24 +0200

    Fixed deprecated use of "new Buffer()"
    https://nodejs.org/api/buffer.html#buffer_new_buffer_string_encoding

--- src/logger/log-req-res.js
+++ src/logger/log-req-res.js
@@ -6,13 +6,13 @@ function createReqResLog (logger) {
     const chunks = [];
 
     res.write = (...restArgs) => {
-      chunks.push(new Buffer(restArgs[0]));
+      chunks.push(Buffer.from(restArgs[0]));
       oldWrite.apply(res, restArgs);
     };
 
     res.end = (...restArgs) => {
       if (restArgs[0]) {
-        chunks.push(new Buffer(restArgs[0]));
+        chunks.push(Buffer.from(restArgs[0]));
       }
       const body = Buffer.concat(chunks).toString('utf8');
 
