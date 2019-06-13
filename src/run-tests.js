41a020996162c5a2c397eee0b91395c1955604a8
David Ruisinger <david.ruisinger@gmail.com>
2019-06-13 13:05:26 +0200

    Allow pattern or filename in tests with short options (#64)
    * Allow pattern or filename in tests with short options
    
    * Bump version

--- src/run-tests.js
+++ src/run-tests.js
@@ -2,7 +2,6 @@ const jestCLI = require('jest');
 const path = require('path');
 
 const runTests = () => {
-  // Remove arguments before --test to not collide with jest args
   const testArgIndex = process.argv.findIndex(arg => /-t$|--test$/.test(arg));
   const argv = [
     ...process.argv.slice(0, 2),
@@ -10,9 +9,11 @@ const runTests = () => {
       __dirname,
       '../test/framework.js'
     )}`,
+    // Remove arguments before --test to not collide with jest args
     ...process.argv.slice(testArgIndex, process.argv.length),
-    ...(/^--/.test(process.argv.slice(-1)[0]) ? ['./'] : []),
-  ].filter(arg => ![ '-t', '--test', '--record' ].includes(arg));
+    // If no pattern or filename is specified (last argument is an option starting with `-`) use `./`
+    ...(/^-/.test(process.argv.slice(-1)[0]) ? ['./'] : []),
+  ].filter(arg => !['-t', '--test', '-r', '--record'].includes(arg));
 
   jestCLI.run(argv);
 };
