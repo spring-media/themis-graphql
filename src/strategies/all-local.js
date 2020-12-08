ca16f2044b1cf100f78db37bfea0f53e90446c56
David Ruisinger <david.ruisinger@gmail.com>
2020-12-08 14:37:51 +0100

    Reverted formatting

--- src/strategies/all-local.js
+++ src/strategies/all-local.js
@@ -7,29 +7,25 @@ const {
 const { insertIfValue, insertIf } = require('../utils');
 
 module.exports = {
-  merge(srcs, { filterSubscriptions, mergedSchemaTransforms = [], logger }) {
-    const { typeDefs, resolvers, remoteSchemas } = srcs.reduce(
-      (p, c) => ({
-        typeDefs: [...p.typeDefs, ...insertIf(!c.remote, c.typeDefs)],
-        resolvers: [...p.resolvers, ...insertIfValue(c.resolvers)],
-        remoteSchemas: [
-          ...p.remoteSchemas,
-          ...insertIf(c.schema && c.mount, c.schema),
-        ],
-        extendTypes: [...p.extendTypes, ...insertIfValue(c.extendTypes)],
-        extendResolvers: [
-          ...p.extendResolvers,
-          ...insertIfValue(c.extendResolvers),
-        ],
-      }),
-      {
-        typeDefs: [],
-        resolvers: [],
-        remoteSchemas: [],
-        extendTypes: [],
-        extendResolvers: [],
-      }
-    );
+  merge (srcs, { filterSubscriptions, mergedSchemaTransforms = [], logger }) {
+    const {
+      typeDefs,
+      resolvers,
+      remoteSchemas,
+    } = srcs
+    .reduce((p, c) => ({
+      typeDefs: [ ...p.typeDefs, ...insertIf(!c.remote, c.typeDefs) ],
+      resolvers: [ ...p.resolvers, ...insertIfValue(c.resolvers) ],
+      remoteSchemas: [ ...p.remoteSchemas, ...insertIf(c.schema && c.mount, c.schema) ],
+      extendTypes: [ ...p.extendTypes, ...insertIfValue(c.extendTypes) ],
+      extendResolvers: [ ...p.extendResolvers, ...insertIfValue(c.extendResolvers) ],
+    }), {
+      typeDefs: [],
+      resolvers: [],
+      remoteSchemas: [],
+      extendTypes: [],
+      extendResolvers: [],
+    });
 
     const localSchema = makeExecutableSchema({
       typeDefs,
@@ -41,7 +37,7 @@ module.exports = {
     });
 
     let schema = mergeSchemas({
-      schemas: [localSchema, ...remoteSchemas],
+      schemas: [ localSchema, ...remoteSchemas ],
     });
 
     if (filterSubscriptions) {
